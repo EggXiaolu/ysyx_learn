b75ac57856be5291571b9fd4f99da6e8de52c3ba
EggXiaolu <2580732276@qq.com>
2024-02-28 17:00:27 +0800

    复习C

---
+++ 预学习/3/1/ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    for(int i=0;i<5;i++){
+        puts("Hello world\n");
+    }
+    return 0;
+}
+
---
+++ 预学习/3/4/ex4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int age=10;
+    int height;
+    printf("I'm %d years old.\n");
+    printf("I'm %d inches tall.\n",height);
+    return 0;
+}
+
