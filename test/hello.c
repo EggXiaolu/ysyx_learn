5f62880d57d1ad8de6b815b8e46e4fb6bd405b2f
EggXiaolu <2580732276@qq.com>
2024-04-18 14:24:47 +0800

    ysyx

--- test/hello.c
+++ test/hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+#include <stdint.h>
 int main()
 {
-    printf("Hello world\n");
+    int32_t a=0x80000010;
+    int32_t b=0xfffec674;
+    printf("0x%08x\n",(a+b)&~1);
     return 0;
 }
 
